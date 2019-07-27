a2abf6172191fc852e331aab3cd6519e29714592
cskumaar1992@gmail.com <sandeepkumaar.c>
2019-07-27 17:15:46 +0530

    created list with add, delete text
    TODO:
    card factory with unique id
    modularise list and card

--- app/index.js
+++ app/index.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState }  from "react";
 
 
 module.exports = function App() {
-	return (<p>Hello world</p>);
-}
+	const [ list, setList] = useState([]);
+
+	const [input, setInput] =  useState("");
+
+	const handleSubmit = function handleSubmit(e) {
+		e.preventDefault();
+		setList(prevList => [...prevList, input]);
+		setInput("");
+	};
+
+	const handleClick = function handleClick(id) {
+		setList(prevList => {
+		  return prevList.filter((item, index) => id !== index)
+			
+		})
+	}
+
+
+	return (
+		<div className="list-container">
+			<form onSubmit={handleSubmit}>
+				<input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+			</form>
+			<ul>
+				{ 
+					list.map((item, index) => (
+						<li key={index}>
+							<span>{item}</span>
+							<button onClick={e => handleClick(index)}>delete</button>
+						</li> 
+					))
+				}
+			</ul>
+		</div>
+	);
+};
+
--- readme.md
+++ readme.md
@@ -1,1 +1,45 @@
 ## Requirements
+
+A board
+
+List
+	- card
+
+Card 
+	- title 
+	- description
+	- comment list
+
+
+Entities:
+Board
+List
+Card
+
+### Design
+
+Every list has add form to add cards
+Every card has a delete button to delete card
+
+Steps:
+List component with text 
+Create text
+Delete a text
+
+Since list cards are passed they need a id to track.
+A Card factory will be responsible for creating cards with unique id
+Change text to card and modularise code
+
+
+create multiple list
+
+move card from list to list
+
+add title description to card
+
+
+add support for comments
+
+Bring redux
+
+add css
